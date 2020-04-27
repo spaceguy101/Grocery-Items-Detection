fc2cb333cd0f15686c503b01876b7b46a8b83098
Shreyas Ponkshe <shreyasponkshe12345@gmail.com>
2020-04-27 21:06:59 +0530

    init

---
+++ precache-manifest.99aaaa68ef03976364e8f971d0cb264c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c765940f6472d666600a9355e51be90a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27c6a0e523b1bec7e5f",
+    "url": "/static/css/main.43224b2e.chunk.css"
+  },
+  {
+    "revision": "1f6f3f44f157bce255bd",
+    "url": "/static/js/2.df6d94da.chunk.js"
+  },
+  {
+    "revision": "f27c6a0e523b1bec7e5f",
+    "url": "/static/js/main.cc38d505.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
